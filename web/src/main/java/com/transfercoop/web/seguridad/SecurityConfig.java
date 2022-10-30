68fede209847cc9edceab613899d76572c1d2ae0
Juan Sebastian <jsebastianlu16@gmail.com>
2022-10-29 19:58:23 -0500

    Se modifico las URL de acceso

--- web/src/main/java/com/transfercoop/web/seguridad/SecurityConfig.java
+++ web/src/main/java/com/transfercoop/web/seguridad/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
-            "/",
+            "/Home/**",
             "/terminos/**",
             "/privacidad/**",
             "/registro/**",  
